d4e55cbe8cebc7a3acabb0a3fe5247c3afff51a3
Divyansh Singh <divyansh.singh@kellogg.northwestern.edu>
2024-04-18 16:48:58 +0000

    Updates

--- kmdb.sql
+++ kmdb.sql
@@ -113,6 +113,14 @@
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
 
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS studios;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS characters;
+
+
+
+
 -- Create new tables, according to your domain model
 -- TODO!
 
@@ -137,3 +145,4 @@
 
 -- The SQL statement for the cast output
 -- TODO!
+
